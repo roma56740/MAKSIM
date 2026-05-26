d8f56c6a415f56e4eed86e89d357dadb2c446713
Roma56740 <r.a.ofizerov@gmail.com>
2026-05-26 16:32:48 +0300

    FixALdsaasdldas

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -5,3 +5,13 @@ from .common import *  # noqa
 from .bills import UBillsCb, ABillsCb
 
 # Если хочешь строгий экспорт — можно позже заменить star-import на явный список __all__
+
+
+from aiogram.filters.callback_data import CallbackData
+
+
+class AdminUsersCb(CallbackData, prefix="ausers"):
+    action: str  # page/view/filter/back
+    page: int = 1
+    tg_id: int = 0
+    status: str = "all"
